2b4f18fd8f2d439b5a77aecbcd13a448200a9acc
suibhne-sweeney <suibhnesweeney@gmail.com>
2024-01-19 21:19:03 +0000

    added sliders to GUI

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -107,33 +107,6 @@ void SimpleEQAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlo
     leftChain.prepare(spec);
     rightChain.prepare(spec);
 
-    //auto chainSettings = getChainSettings(apvts);
-
-    //updatePeakFilter(chainSettings);
-
-    //auto lowCutCoefficients = juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(
-    //    chainSettings.lowCutFreq, 
-    //    sampleRate, 
-    //    2 * (chainSettings.lowCutSlope + 1)
-    //);
-
-    //auto& leftLowCut = leftChain.get<ChainPositions::LowCut>();
-    //updateCutFilter(leftLowCut, lowCutCoefficients, chainSettings.lowCutSlope);
-
-    //auto& rightLowCut = rightChain.get<ChainPositions::LowCut>();
-    //updateCutFilter(rightLowCut, lowCutCoefficients, chainSettings.lowCutSlope);
-
-    //auto highCutCoefficients = juce::dsp::FilterDesign<float>::designIIRLowpassHighOrderButterworthMethod(
-    //    chainSettings.highCutFreq,
-    //    sampleRate,
-    //    2 * (chainSettings.highCutFreq + 1)
-    //);
-
-    //auto& leftHighCut = leftChain.get<ChainPositions::HighCut>();
-    //auto& rightHighCut = rightChain.get<ChainPositions::HighCut>();
-
-    //updateCutFilter(leftHighCut, highCutCoefficients, chainSettings.highCutSlope);
-    //updateCutFilter(rightHighCut, highCutCoefficients, chainSettings.highCutSlope);
     updateFilters();
 }
 
@@ -184,35 +157,6 @@ void SimpleEQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juc
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; i++)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    //auto chainSettings = getChainSettings(apvts);
-
-    //updatePeakFilter(chainSettings);
-
-
-    //auto cutCoefficients = juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(
-    //    chainSettings.lowCutFreq,
-    //    getSampleRate(),
-    //    2 * (chainSettings.lowCutSlope + 1)
-    //);
-
-    //auto& leftLowCut = leftChain.get<ChainPositions::LowCut>();
-    //updateCutFilter(leftLowCut, cutCoefficients, chainSettings.lowCutSlope);
-
-    //auto& rightLowCut = rightChain.get<ChainPositions::LowCut>();
-    //updateCutFilter(rightLowCut, cutCoefficients, chainSettings.lowCutSlope);
-
-    //auto highCutCoefficients = juce::dsp::FilterDesign<float>::designIIRLowpassHighOrderButterworthMethod(
-    //    chainSettings.highCutFreq,
-    //    getSampleRate(),
-    //    2 * (chainSettings.highCutFreq + 1)
-    //);
-
-    //auto& leftHighCut = leftChain.get<ChainPositions::HighCut>();
-    //auto& rightHighCut = rightChain.get<ChainPositions::HighCut>();
-
-    //updateCutFilter(leftHighCut, highCutCoefficients, chainSettings.highCutSlope);
-    //updateCutFilter(rightHighCut, highCutCoefficients, chainSettings.highCutSlope);
-
     updateFilters();
 
     juce::dsp::AudioBlock<float> block(buffer);
@@ -237,8 +181,8 @@ bool SimpleEQAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* SimpleEQAudioProcessor::createEditor()
 {
-    // return new SimpleEQAudioProcessorEditor (*this);
-    return new juce::GenericAudioProcessorEditor(*this);
+    return new SimpleEQAudioProcessorEditor (*this);
+    //return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
@@ -247,12 +191,22 @@ void SimpleEQAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+
+    juce::MemoryOutputStream mos(destData, true);
+    apvts.state.writeToStream(mos);
+
 }
 
 void SimpleEQAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+    if (tree.isValid()) {
+        apvts.replaceState(tree);
+        updateFilters();
+    }
+
 }
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts) {
